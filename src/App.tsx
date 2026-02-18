ceacc82f3c62b17a8cfa8a27d9da75089134aad4
Mohaneshwari78 <mohaneshwaricsd@gmail.com>
2026-02-18 14:16:47 +0530

    about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { HomePage } from "./pages/HomePage";
+// import { ShopPage } from "./pages/ShopPage"; // or wherever your shop page is
+// import { BuyNowPage } from "./pages/BuyNowPage";
+// import { ContactPage } from "./pages/ContactPage";
+
+// export function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/shop" element={<ShopPage />} />
+//         <Route path="/buy" element={<BuyNowPage />} />
+//         <Route path="/contact" element={<ContactPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { ShopPage } from "./pages/ShopPage"; // or wherever your shop page is
 import { BuyNowPage } from "./pages/BuyNowPage";
 import { ContactPage } from "./pages/ContactPage";
+import Aboutus from "./pages/About";
 
 export function App() {
   return (
@@ -12,6 +34,7 @@ export function App() {
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/buy" element={<BuyNowPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/about" element={<Aboutus />} />
       </Routes>
     </BrowserRouter>
   );
